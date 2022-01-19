bbeb42ec4536ac0a171ffe3bde53780cab3f971f
Susanna Yossef <susannayossef98@gmail.com>
2022-01-19 16:22:56 +0100

    update

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,110 +1,52 @@
 import styles from './Product.module.scss';
-import clsx from 'clsx';
-import Button from '../Button/Button';
-// import productsData from '../../data/products';
-import { useState } from 'react';
 import PropTypes from 'prop-types';
+import { useState, useMemo } from 'react';
+import ProductImage from '../ProductImage/ProductImage';
+import ProductForm from '../ProductForm/ProductForm';
 
-const Product = (props) => {
-  const [currentColor, setCurrentColor] = useState(props.colors[0]);
-  const [currentSize, setCurrentSize] = useState(props.sizes[0].name);
+const Product = ({ name, title, basePrice, colors, sizes }) => {
 
-  const getPrice = () => {
-    return (
-      props.basePrice +
-      props.sizes.find((element) => element.name === currentSize)
-        .additionalPrice
-    );
-  };
+  const [currentColor, setCurrentColor] = useState(colors[0]);
+  const [currentSize, setCurrentSize] = useState(sizes[0].name);
 
-  const prepareColorClassName = (color) => {
-    return styles[
-      'color' + color[0].toUpperCase() + color.substr(1).toLowerCase()
-    ];
-  };
+  const getPrice = useMemo(() => {
+    console.log('price changed')
+    const clickedSize = sizes.find(element => element.name === currentSize)
 
+    return basePrice + clickedSize.additionalPrice;
+  }, [currentSize, basePrice, sizes]);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    console.log('Summary')
+    console.log('===============')
+    console.log('Name:  ' + title)
+    console.log('Price:  ' + getPrice)
+    console.log('Size:  ' + currentSize)
+    console.log('Color:  ' + currentColor)
+  }
+  
   return (
     <article className={styles.product}>
-      <div className={styles.imageContainer}>
-        <img
-          className={styles.image}
-          alt={`${props.title}`}
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-${props.name}--${currentColor}.jpg`}
-        />
-      </div>
+      <ProductImage name={name} title={title} currentColor={currentColor} />
       <div>
         <header>
-          <h2 className={styles.name}> {props.title} </h2>
-          <span className={styles.price}>Price: {getPrice()}$</span>
+          <h2 className={styles.name}>{title}</h2>
+          <span className={styles.price}>{getPrice}</span>
         </header>
-        <form>
-          <div className={styles.sizes}>
-            <h3 className={styles.optionLabel}>Sizes</h3>
-            <ul className={styles.choices}>
-              {props.sizes.map((size, index) => {
-                return (
-                  <li key={index}>
-                    <button
-                      type='button'
-                      className={clsx(
-                        size.name,
-                        size.name === currentSize && styles.active
-                      )}
-                      onClick={() => {
-                        console.log(currentSize);
-                        setCurrentSize(props.sizes[index].name);
-                      }}
-                    >
-                      {size.name}
-                    </button>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-          <div className={styles.colors}>
-            <h3 className={styles.optionLabel}>Colors</h3>
-            <ul className={styles.choices}>
-              {props.colors.map((item, index) => (
-                <li key={item}>
-                  <button
-                    type='button'
-                    className={clsx(
-                      prepareColorClassName(item),
-                      item === currentColor && styles.active
-                    )}
-                    onClick={() => {
-                      setCurrentColor(props.colors[index]);
-                    }}
-                  />
-                </li>
-              ))}
-            </ul>
-          </div>
-          <Button className={styles.button}>
-            <span className='fa fa-shopping-cart' />
-          </Button>
-        </form>
+        <ProductForm handleSubmit={handleSubmit} sizes={sizes} colors={colors} currentColor={currentColor} currentSize={currentSize} setCurrentColor={setCurrentColor} setCurrentSize={setCurrentSize}/>
       </div>
     </article>
-  );
+  )
 };
 
 Product.propTypes = {
-  title: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  colors: PropTypes.arrayOf(PropTypes.string).isRequired,
+  title: PropTypes.string.isRequired,
   basePrice: PropTypes.number.isRequired,
-  // sizes: PropTypes.shape({
-  //   additionalPrice: PropTypes.number.isRequired,
-  //   name: PropTypes.string.isRequired,
-  // }),
-  sizes: PropTypes.arrayOf(
-    PropTypes.shape({
-      additionalPrice: PropTypes.number.isRequired,
-      name: PropTypes.string.isRequired,
-    })
-  ),
+  colors: PropTypes.array.isRequired,
+  sizes: PropTypes.array.isRequired
 };
 
 export default Product;
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -3,21 +3,11 @@ import productsData from '../../data/products';
 import Product from '../Product/Product';
 
 const Products = () => {
-  const [products]  = useState(productsData);
+  const [products] = useState(productsData);
 
   return (
     <section>
-      {products.map((product) => (
-        <Product
-          key={product.id}
-          id={product.id}
-          name={product.name}
-          title={product.title}
-          colors={product.colors}
-          sizes={product.sizes}
-          basePrice={product.basePrice} 
-        />
-      ))}
+      {products.map(product => <Product key={product.id} {...product}  />)}
     </section>
   );
 };
