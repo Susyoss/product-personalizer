ab68d05643d230e009d7987bac2d168c8e68ff79
Susanna Yossef <susannayossef98@gmail.com>
2022-02-07 17:21:46 +0100

    update

--- src/components/ProductImage/ProductImage.js
+++ src/components/ProductImage/ProductImage.js
@@ -1,14 +1,22 @@
 import styles from './ProductImage.module.scss';
+import PropTypes from 'prop-types';
 
-const ProductImage = ({ name, title, currentColor }) => {
+const ProductImage = ({ title, name, currentColor }) => {
   return (
     <div className={styles.imageContainer}>
-      <img 
+      <img
         className={styles.image}
         alt={title}
-        src={`${process.env.PUBLIC_URL}/images/products/shirt-${name}--${currentColor}.jpg`} />
+        src={`${process.env.PUBLIC_URL}/images/products/shirt-${name}--${currentColor}.jpg`}
+      />
     </div>
   );
-}
+};
+
+ProductImage.propTypes = {
+  title: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  currentColor: PropTypes.string.isRequired,
+};
 
 export default ProductImage;
